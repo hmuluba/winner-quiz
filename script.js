36924575f7d639c838848094de2e88ec39214569
Mulher <hmuluba@gmail.com>
2020-09-26 22:02:49 +0100

    winner quiz

---
+++ script.js
@@ -0,0 +1,235 @@
+const quizData = [
+    {
+        question: "01. Donner la date où le visionnaire de la chapelle des vainqueurs internationale avait reçu le mandant de la libération ?    ",
+        a: "Du 1er au 02 Mai 1981",
+        b: "Du 04 au 11 Decembre 1999",
+        c: "Le 2 Mai 1987",
+        d: "Aucune bonne réponse",
+        correct: "a",
+    },
+    {
+        question: "02. Dans quelle ville du Nigeria, la premier Eglise de la Chapelle des vainqueurs avait été implanté ? ",
+        a: "Lagos",
+        b: "Kinshasa",
+        c: "Otta",
+        d: "Kaduna",
+        correct: "d",
+    },
+    {
+        question: "03. Comment s’appelle le premier culte de dimanche du mois à la Chapelle des vainqueurs ",
+        a: "Culte d’entrée prophétique",
+        b: "Culte d'onction d'huile",
+        c: "Culte de bénédiction du peuple de Dieu",
+        d: "Culte du 1er Jour du mois",
+        correct: "a",
+    },
+    {
+        question: "04. Donner le nom complet du visionnaire de l’église Chapelle des vainqueurs internationale ",
+        a: "David Olinka Oyedepo",
+        b: "David Oyuroba Oyedepo",
+        c: "David Oremu Oyedepo",
+        d: "David Olani Oyedepo",
+        correct: "d",
+    },
+    {
+        question: "05. Quel est le mystère utilisé lors de nos cultes de mi- semaine ?    ",
+        a: "Le mystère de la sainte scène",
+        b: "Le mystère de Lavement de Pied",
+        c: "Le mystère de la Parole",
+        d: "Le mystère de la dance",
+        correct: "a",
+    },
+    {
+        question: "06. Que signifie IBPF? ",
+        a: "Institut Bilingue Parole de Foi",
+        b: "Institut Biblique Prophétique de la Foi",
+        c: "Institut Biblique Parole de Foi",
+        d: "Aucune bonne réponse",
+        correct: "c",
+    },
+    {
+        question: "07. Donner la date où le visionnaire de la chapelle des vainqueurs internationale avait reçu le mandant de la libération ?    ",
+        a: "Du 1er au 02 Mai 1981",
+        b: "Du 04 au 11 Decembre 1999",
+        c: "Le 2 Mai 1987",
+        d: "Aucune bonne réponse",
+        correct: "a",
+    },
+    {
+        question: "08. quoi consiste le mandat de la prospérité que le visionnaire de l’église Chapelle des Vainqueurs avait reçu de Dieu ? ",
+        a: "Parle de moi au peuple du nigeria",
+        b: "Rentre au pays et élève-moi un peuple riche ",
+        c: "La moisso, de l'afrique est mure",
+        d: "L'europe est le lieu que j'ai choisi",
+        correct: "b",
+    },
+    {
+        question: "09. En quelle année l’Apôtre établi à la chapelle des vainqueurs avait reçu le mandat de la prospérité ?    ",
+        a: "En 1993",
+        b: "En 1986",
+        c: "En 1966",
+        d: "En 1995",
+        correct: "d",
+    },
+    {
+        question: "10. Comment appelle-t-on le livre qui renferme les thèmes de chaque mois de l’année ?  ",
+        a: "Les douze apotre",
+        b: "Les douze piliers",
+        c: "Les douze Thème du mois ",
+        d: "Les douze commandement",
+        correct: "b",
+    },
+    {
+        question: "11. Comment s’appelle le plus grand rassemblement annuel dans le monde à la chapelle des vainqueurs ?    ",
+        a: "Siloé",
+        b: "Shiloh",
+        c: "Campement",
+        d: "Pâques",
+        correct: "b",
+    },
+    {
+        question: "12. Comment s’appelle en sigle le premier niveau de l’Institut Biblique parole de foi ?  ",
+        a: "I.B.P.F",
+        b: "L.C.C",
+        c: "B.C.C",
+        d: "L.D.C",
+        correct: "c",
+    },
+    {
+        question: "13. Quel est le mystère utiliser à l’église lors du culte de transfert ?    ",
+        a: "Le manteau d'Elie",
+        b: "La veste de Bishop",
+        c: "Le manteau d'onction",
+        d: "Le manteau prohétique",
+        correct: "d",
+    },
+    {
+        question: "14. Quelles sont les quatre questions essentielles du Mandat de libération ?",
+        a: "La tâche, La cible, L’outil et Le temps",
+        b: "Le père, Le Fils, Le Saint-Esprit, Amen",
+        c: "Le Pourquoi, Le comment, Le Quand, Le Où",
+        d: "Aucune bonne réponse",
+        correct: "a",
+    },
+    {
+        question: "15. En quelle année fut achevé la construction du tabernacle de la foi ?    ",
+        a: "En 2020",
+        b: "En 2007",
+        c: "En 1996",
+        d: "En 1999",
+        correct: "d",
+    },
+    {
+        question: "16. Quel est le texte biblique sur la question doctrinale de l’Eglise ? ",
+        a: "2 Timothée 3 :16-17 ",
+        b: "2 Chronique 3 :16-17",
+        c: "2 Rois 3 :16-17",
+        d: "2 Thessaloniciens 3 :16-17",
+        correct: "a",
+    },
+    {
+        question: "17. Donner la date à laquelle le Mandat entra en vigueur ? ",
+        a: "Le 7 Septembre 2012",
+        b: "Le 7 Juin 1988",
+        c: "Le 7 Mai 2010",
+        d: "Le 7 Janvier 2001",
+        correct: "a",
+    },
+    {
+        question: "18. Cette année l’église Chapelle des vainqueurs dans le monde totalise combien d’année depuis sa création ?    ",
+        a: "31 ans",
+        b: "30 ans",
+        c: "38 ans",
+        d: "39 ans",
+        correct: "d",
+    },
+    {
+        question: "19. Quel est la date d’anniversaire de l’église Chapelle des Vainqueurs  ?  ",
+        a: "Le 3 Janvier",
+        b: "Le 30 Juillet",
+        c: "Le 2 Mai",
+        d: "Le 25 Décembre",
+        correct: "d",
+    },
+    {
+        question: "20. Quel est l’instrument que Dieu avait donné à son serviteur pour exécuter le mandat de libération ? ",
+        a: "Le Mandat de libération ",
+        b: "La Parole de la foi",
+        c: "La parole de Dieu",
+        d: "La parole prophétique",
+        correct: "c",
+    }
+    
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+   
+    
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+            <h2>Liste des bonnes réponses</h2>
+            <h2> 1. a / 2. d / 3. a  / 4. d / 5. a / 6. c / 7. a / 8. b / 9. d / 10. b / 11. b / 12. c / 13. d / 14. a / 15. d / 16. a / 17. a / 18. d / 19. d / 20. c </h2>       
+        
+            <h2>Vous avez répondu correctement à ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Revenir aux questionnaires</button>
+            `;
+        }
+    }
+});
